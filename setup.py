6f2acc5dbd571a7c5248e20a8ce2deb0bff98a4d
cesare-spinoso <cesare.spinoso@gmail.com>
2024-02-09 09:48:07 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,45 @@
+from distutils.core import setup
+
+# README file contents
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="RSA-Summ",
+    version="0.1dev",
+    packages=[
+        "src",
+    ],
+    license="MIT",
+    description="RSA Summ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Cesare Spinoso-Di Piano",
+    author_email="cesare.spinoso-dipiano@mail.mcgill.ca",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "tabulate",
+        "matplotlib",
+        "scikit-learn",
+        "nltk",
+        "spacy",
+        "gensim",
+        "torch",
+        "rank_bm25",
+        "bs4",
+        "plotly",
+        "omegaconf",
+        "hydra-core",
+        "jsonlines",
+        "transformers",
+        "sentence-transformers",
+        "tqdm",
+        "seaborn",
+        "scipy",
+        "ipython",
+        "protobuf",
+    ],
+)
