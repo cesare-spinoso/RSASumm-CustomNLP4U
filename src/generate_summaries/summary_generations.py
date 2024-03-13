83d09142844f4cc7e8e17651d546de6e45e5650a
cesare-spinoso <cesare.spinoso@gmail.com>
2024-03-12 20:49:50 -0400

    save

--- src/generate_summaries/summary_generations.py
+++ src/generate_summaries/summary_generations.py
@@ -56,7 +56,6 @@ def load_model(cfg):
 @hydra.main(
     version_base=None,
     config_path=os.path.join(SRC_DIRECTORY, "generate_summaries", "conf"),
-    config_name="config3",
 )
 @main_decorator
 def main(run_name: str, cfg: DictConfig) -> None:
